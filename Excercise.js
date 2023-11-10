d9c464d231b75655fdad886ca421007bbb747dc5
Ashish <ash.ishu025@gmail.com>
2023-11-10 22:42:24 +0530

    excercise

---
+++ Excercise.js
@@ -0,0 +1,75 @@
+//1. What would be answer to this - why
+console.log("Before: ", a);
+var a = 10;
+console.log("After: ", a);
+
+
+//2. Global Scope - what should be the issue - in logging - a
+
+function hoistExample() {
+  var a;
+  a = 10;
+}
+
+hoistExample();
+console.log(a); 
+
+//3. Figure out the problem
+
+testFunc();
+var testFunc = function funcHoist() {
+  console.log("I am being hoisted.");
+}	
+
+//4. SetTimeouts - concurrent executions
+
+setTimeout(function(){
+    console.log("first timeout");
+    setTimeout(function(){
+        console.log("inner timeout");			
+    },0);	
+    
+    setTimeout(function(){
+        console.log("second timeout");
+    },100);
+    
+    setTimeout(function(){
+        console.log("third timeout");
+    },100);
+},100);
+
+//5. Function Hoisting - see and understand the out put
+console.log(funcs);
+var funcs;
+function funcs(){
+    funcs = 3;
+}
+funcs();
+console.log(funcs);
+
+//6. Function Hoisting - see and understand the out put
+console.log(funcs);
+var funcs;
+var funcs = function (){
+    funcs = 5;
+}
+funcs();
+console.log(funcs);
+
+
+//7. See the OutPut of the following and try finding why it is so
+
+function sum(x, y) {
+  if (y !== undefined) {
+    return x + y;
+  } else {
+    return function(y) { return x + y; };
+  }
+}
+
+console.log(sum(2,3));   // Outputs -
+console.log(sum(2)(3));  // Outputs -
+
+
+//8. Create a function to populate user details. 
+//	func - populateDetail prints Adress and accepts a callBackFunction which prints Name, Age and Topic
